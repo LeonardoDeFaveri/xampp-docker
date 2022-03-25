c17a1a2e56390a2b3bf9d30d87c2704e209a5e8e
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2022-03-25 14:43:42 +0100

    first commit

---
+++ start-xampp.sh
@@ -0,0 +1,6 @@
+#! /bin/env bash
+docker run --detach -ti -p 80:80 -p 443:443 -p 3306:3306 --name xampp \
+	-v /media/leonardo/various/esercizi-uni/prog-web/data:/www \
+	-v /media/leonardo/various/esercizi-uni/prog-web/data/db:/opt/lampp/var/mysql/ \
+	--network=tomcat-docker_xampp-tomcat \
+	my/xampp
