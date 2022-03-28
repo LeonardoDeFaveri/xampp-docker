b2aeee699f955a8313c7625f94dedbf760856418
LeonardoDeFaveri <leonardo.defaveri01@gmail.com>
2022-03-28 09:10:28 +0200

    Fixed bug that didn't make mysql start; data/db doesn't refere anymore to mysql

--- start-xampp.sh
+++ start-xampp.sh
@@ -1,6 +1,8 @@
 #! /bin/env bash
-docker run --detach -ti -p 80:80 -p 443:443 -p 3306:3306 --name xampp \
+CONTAINER_NAME=xampp
+
+docker run --detach -ti -p 80:80 -p 443:443 -p 3306:3306 --name ${CONTAINER_NAME} \
 	-v /media/leonardo/various/esercizi-uni/prog-web/data:/www \
-	-v /media/leonardo/various/esercizi-uni/prog-web/data/db:/opt/lampp/var/mysql/ \
+	--mount "source=${CONTAINER_NAME}-vol,destination=/opt/lampp/var/mysql/" \
 	--network=tomcat-docker_xampp-tomcat -e LANG=C.UTF-8 \
 	my/xampp
